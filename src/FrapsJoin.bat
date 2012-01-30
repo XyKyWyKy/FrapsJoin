d1038f0463c19357df0d2dc2347783edacf87236
Lindsay Bigelow <lindsaybigelow@cad2data.com>
2012-01-30 17:40:47 -0500

    added usage comment

--- src/FrapsJoin.bat
+++ src/FrapsJoin.bat
@@ -1,2 +1,9 @@
+::** BAT file for FrapsJoin
+::
+::usage: 
+::  - right click any AVI file
+::  - "Open With...", browse here, Enter
+::  - this BAT file will now appear in the "Open With" list for AVI files
+::
 start wscript "%~dp0\FrapsJoin.vbs" %1
 exit 
